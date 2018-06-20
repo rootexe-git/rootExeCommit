675e5eb5acf0b9f50ee9e52980dc7d51e03ace6a
Yash Zolmajdi <yash.zolmajdi@plutora.com>
2018-06-20 21:59:09 +1000

    init

---
+++ web/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './css/App.css';
+
+import Base from './base';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App"><Base /></div>
+    );
+  }
+}
+
+export default App;
